dbdd8e636715ebf6a69695df6fda837eb5bbad46
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-05-26 18:53:39 +0530

    Deleted 01-arr.java and added new files

---
+++ Array/Array1.java
@@ -0,0 +1,11 @@
+// Creating an Array in Java
+package Array;
+import java.util.*;
+public class Array1 {
+  public static void main(String args[]){
+    int numbers[] = new int[10];
+    String names[] = {"Sibangi", "Resma", "Seetal", "Ritika", "Subhashri"};
+    int moreNumbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  }
+}
+
