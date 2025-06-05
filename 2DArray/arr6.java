4d6c837681836a672e8193aa5b8dc2da66bddf0e
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-05 22:52:46 +0530

    brute force approach for diagonal sum code

---
+++ 2DArray/arr6.java
@@ -0,0 +1,27 @@
+//Brute force approach for diagonal sum
+public class arr6 {
+  public static int diagonalSum(int matrix[][]){
+    int sum=0;
+    for(int i=0;i<matrix.length;i++){
+      for(int j=0;j<matrix[0].length;j++){
+        //primary diagonal
+        if(i==j){
+          sum+=matrix[i][j];
+        }
+        //secondary diagonal
+        else if(i+j==matrix.length-1){
+          sum+=matrix[i][j];
+        }
+      }
+    }
+    System.out.println("Sum is: "+sum);
+    return sum;
+  }
+  public static void main(String args[]){
+    int matrix[][] = {{1,2,3,4},
+                      {5,6,7,8},
+                      {9,10,11,12},
+                      {13,14,15,16}};
+    diagonalSum(matrix);
+  }
+}
