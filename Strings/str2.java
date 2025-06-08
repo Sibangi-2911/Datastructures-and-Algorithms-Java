569d778f438174cc55fc0049c6b4c78acd0e8914
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-08 23:02:28 +0530

    concatenation and finding length in strings

---
+++ Strings/str2.java
@@ -0,0 +1,13 @@
+//length and concatenation in strings
+public class str2 {
+  public static void main(String args[]){
+    //calculate length
+    String name = "Sibangi Boxipatro";
+    System.out.println(name.length());
+    //concatenation
+    String firstname = "Sibangi";
+    String lastname = "Boxipatro";
+    String FullName = firstname+" "+lastname;
+    System.out.println(FullName);
+  }
+}
