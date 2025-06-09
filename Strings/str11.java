73c961ae18980311bc8b3bc04a03aa3722892916
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-09 21:44:24 +0530

    String compression example

---
+++ Strings/str11.java
@@ -0,0 +1,22 @@
+//String Compression - O(n)
+public class str11 {
+  public static String compressString(String str){
+    StringBuilder sb = new StringBuilder("");
+    for(int i=0;i<str.length();i++){
+      Integer count=1;
+      while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
+        count++;
+        i++;
+      }
+      sb.append(str.charAt(i));
+      if(count>1){
+        sb.append(count.toString());
+      }
+    }
+    return sb.toString();
+  }
+  public static void main(String args[]){
+    String str ="aaabbbcccdddeee";
+    System.out.println(compressString(str));
+  }
+}
