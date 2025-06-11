189db4a048d303a3c84a22fae26333935084f9be
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-11 21:51:50 +0530

    added examples of all bitwise operators

---
+++ BitManipulation/bit1.java
@@ -0,0 +1,12 @@
+//Bitwise operators
+public class bit1 {
+  public static void main(String args[]){
+    System.out.println(5&6); //and
+    System.out.println(5|6);//or
+    System.out.println(5^6);//xor
+    System.out.println(~5);//complement
+    System.out.println(~0);
+    System.out.println(5<<2);//leftshift
+    System.out.println(5>>2);//rightshift
+  }
+}
