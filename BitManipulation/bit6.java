8691dffefa52dd8d6fc46287ca0f6f68bd3ff798
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-13 11:35:13 +0530

    code to update ith bit

---
+++ BitManipulation/bit6.java
@@ -0,0 +1,14 @@
+public class bit6 { 
+  public static int clearIthBit(int n, int i){
+    int bitMask = ~(1<<i);
+    return n & bitMask;
+  }
+  public static int updateIthBit(int n, int i, int newbit){
+    n = clearIthBit(n, i);
+    int bitMask = newbit<<i;
+    return n | bitMask;
+  }
+  public static void main(String args[]){
+    System.out.println(updateIthBit(10, 2, 1));
+  }
+}
