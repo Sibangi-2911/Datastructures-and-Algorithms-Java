fc43ac735ccc580742ca28bd0c57091fa9fe6b4d
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-21 10:13:26 +0530

    Convert uppercase to lowercase using bitmanipulation

---
+++ BitManipulation/bit14.java
@@ -0,0 +1,8 @@
+//Convert uppercase characters to lowercase characters 
+public class bit14 {
+  public static void main(String args[]){
+    for (char ch ='A'; ch<='Z';ch++){
+      System.out.print((char)(ch | ' '));
+    }
+  }
+}
