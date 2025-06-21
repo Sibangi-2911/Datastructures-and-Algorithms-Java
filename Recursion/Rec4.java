011d89a359dbd47204afd7871b9e233cad12fd26
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-21 15:34:34 +0530

    code to Print sum of first n natural numbers

---
+++ Recursion/Rec4.java
@@ -0,0 +1,15 @@
+//Print sum of first n natural numbers
+public class Rec4 {
+  public static int printSum(int n){
+    if(n==1){
+      return 1;
+    }
+    int fnm = printSum(n-1);
+    int fn = n + fnm;
+    return fn;
+  }
+  public static void main(String args[]){
+    int n =5;
+    System.out.println(printSum(n));
+  }
+}
