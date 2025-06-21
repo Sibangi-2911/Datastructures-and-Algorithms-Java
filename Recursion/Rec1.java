ab1481c9a6a1669b5c5afe790dc1ebdf5dcb6d0d
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-21 15:21:16 +0530

    code to print numbers from 10 to 1

--- Recursion/Rec1.java
+++ Recursion/Rec1.java
@@ -1,3 +1,15 @@
+// print numbers from 10 to 1
 public class Rec1 {
-  
+  public static void printDec(int n){
+    if(n==1){
+      System.out.println(n);
+      return;
+    }
+    System.out.print(n+" ");
+    printDec(n-1);
+  }
+  public static void main(String args[]){
+    int n =10;
+    printDec(n);
+  }
 }
