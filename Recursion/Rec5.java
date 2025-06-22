90ca4899eb9a548ff185b7eac431b4aff7200feb
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-22 22:51:42 +0530

    Fibonacci code added

---
+++ Recursion/Rec5.java
@@ -0,0 +1,18 @@
+public class Rec5 {
+  public static int fibonacci(int n){
+    if(n==0){
+      return 0;
+    }
+    else if(n==1){
+      return 1;
+    }
+    int fibnm1 = fibonacci(n-1);
+    int fibnm2 = fibonacci(n-2);
+    int fibn = fibnm1 + fibnm2;
+    return fibn;
+  }
+  public static void main(String args[]){
+    int n =25;
+    System.out.println(fibonacci(n));
+  }
+}
