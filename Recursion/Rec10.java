12ef884897a919e8d2bdc3026f9902a8ecbbcfc2
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-23 23:04:20 +0530

    code to print x to the power n

---
+++ Recursion/Rec10.java
@@ -0,0 +1,18 @@
+//optimized code that is O(log n) time complexity for x to the power n
+public class Rec10 { 
+  public static int optimizedPower(int x, int n){
+    if(n==0){
+      return 1;
+    }
+    int halfPower = optimizedPower(x, n/2);
+    int halfPowerSq = halfPower*halfPower;
+    if(n%2!=0){
+      halfPowerSq = x * halfPowerSq;
+    }
+    return halfPowerSq;
+  }
+  public static void main(String args[]){
+    System.out.println(optimizedPower(2,3));
+
+  }
+}
