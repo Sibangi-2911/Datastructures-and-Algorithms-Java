a43485013a6976e3b61254f1cb86c4e627bfba34
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-25 23:42:04 +0530

    Print all vbinary strings of sizen without consecutive ones

---
+++ Recursion/Rec14.java
@@ -0,0 +1,17 @@
+//Print all vbinary strings of sizen without consecutive ones
+public class Rec14 {
+  public static void printBinaryStrings(int n, int lastplace, String str){
+    //base case
+    if(n==0){
+      System.out.println(str);
+      return;
+    }
+    printBinaryStrings(n-1, 0, str+"0");
+    if(lastplace==0){
+      printBinaryStrings(n-1, 1, str+"1");
+    }
+  }
+  public static void main(String args[]){
+    printBinaryStrings(3, 0, "");
+  }
+}
