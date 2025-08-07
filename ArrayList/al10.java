33bfc393e05c50ec553699864db76f3042f05f3d
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-08-07 23:15:49 +0530

    find if any pair in sorted arraylist has a target sum - bruteforce approach - O(n^2)

---
+++ ArrayList/al10.java
@@ -0,0 +1,24 @@
+//find if any pair in sorted arraylist has a target sum - bruteforce approach - O(n^2)
+import java.util.*;
+public class al10 {
+  public static boolean pairSum(ArrayList <Integer> list, int target){
+    for(int i=0;i<list.size();i++){
+      for(int j=i+1;j<list.size();j++){
+        if(list.get(i)+list.get(j)==target){
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  public static void main(String args[]){
+    ArrayList <Integer> list = new ArrayList<>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+    int target = 5;
+    System.out.println(pairSum(list, target));
+  }
+}
