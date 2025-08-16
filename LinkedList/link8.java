8fdeac0cbb0d18ea2718230af0fbb74b328be29b
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-08-16 23:15:29 +0530

    code to Detect a loop/cycle in  a LL

---
+++ LinkedList/link8.java
@@ -0,0 +1,34 @@
+//Detect a loop/cycle in  a LL- Floyd's cycle finding algorithm
+public class link8 {
+  public static class Node{
+    int data;
+    Node next;
+    public Node(int data){
+      this.data = data;
+      this.next=null;
+    }
+  }
+  public static Node head;
+  public static Node tail;
+  public static int size;
+
+  public static boolean isCycle(){
+    Node slow = head;
+    Node fast = head;
+    while(fast!=null && fast.next!=null){
+      slow = slow.next;
+      fast = fast.next.next;
+      if(slow==fast){
+        return true; //cycle exists
+      }
+    }
+    return false;
+  }
+  public static void main(String args[]){
+    head = new Node(1);
+    head.next = new Node(2);
+    head.next.next = new Node(3);
+    head.next.next.next = head;
+    System.out.println(isCycle());
+  }
+}
