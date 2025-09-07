3cf3f2cd071ac8b8c2158d4b58b22416d519c474
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-09-07 21:14:35 +0530

    implementation of stack using collections framework

---
+++ Stack/stack3.java
@@ -0,0 +1,14 @@
+//using the collections framework for stack
+import java.util.*;
+public class stack3 {
+  public static void main(String args[]){
+    Stack<Integer> s = new Stack<>();
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    while(!s.isEmpty()){
+      System.out.println(s.peek());
+      s.pop();
+    }
+  }
+}
