af22d317e2d684add3759e028be2d5a956fac2e7
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-09-10 21:59:36 +0530

    Circular linked list operation

---
+++ LinkedList/link15.java
@@ -0,0 +1,13 @@
+//Circular Linked list
+public class link15 {
+  public class Node{
+    int data;
+    Node next;
+    public Node(int data){
+      this.data = data;
+    }
+  }
+  public static void main(String args[]){
+
+  }
+}
