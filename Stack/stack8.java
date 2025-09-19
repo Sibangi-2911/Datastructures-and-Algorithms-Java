b21e2d75c00ba568017993185c5af3a5aa1c59a0
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-09-19 18:37:22 +0530

    code to find the next Greater element for ech in an array

---
+++ Stack/stack8.java
@@ -0,0 +1,31 @@
+//code to find the next Greater element for ech in an array
+import java.util.*;
+public class stack8 {
+  public static void main(String args[]){
+    int arr[] = {6,8,0,1,3};
+    Stack<Integer> s = new Stack<>();
+    int nextGreater [] = new int[arr.length];
+
+    for(int i=arr.length-1;i>=0;i--){
+      //while loop
+      while(!s.isEmpty() && arr[s.peek()] <= arr[i]){
+        s.pop();
+      }
+
+      //if-else
+      if(s.isEmpty()){
+        nextGreater[i] = -1;
+      } else{
+        nextGreater[i] = arr[s.peek()];
+      }
+
+      //push element
+      s.push(i);
+    }
+
+    for(int i=0; i<nextGreater.length;i++){
+      System.out.print(nextGreater[i]+ " ");
+    }
+    System.out.println();
+  }
+}
