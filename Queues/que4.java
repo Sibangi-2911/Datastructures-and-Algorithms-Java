62552c4ffbae07b9b226aa126ea8c852ea9007c9
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-10-12 21:55:41 +0530

    modified the comment

---
+++ Queues/que4.java
@@ -0,0 +1,12 @@
+//Code for Implementation of Queue using Java Collection Framework
+import java.util.*;
+public class que4 {
+  public static void main(String args[]){
+    Queue<Integer> q = new LinkedList<>();
+    q.add(1);
+    q.add(2);
+    q.add(3);
+
+    
+  }
+}
