7e1f3be291971897b3c7db55b01be0ea5b7eee34
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-10-13 21:08:49 +0530

    Code for Implementation of Queue using Java Collection Framework with class ArrayDeque

---
+++ Queues/que5.java
@@ -0,0 +1,17 @@
+//Code for Implementation of Queue using Java Collection Framework with class ArrayDeque
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class que5 {
+  public static void main(String args[]){
+    Queue<Integer> q = new ArrayDeque<>();
+    q.add(1);
+    q.add(2);
+    q.add(3);
+    while(!q.isEmpty()){
+      System.out.println(q.peek());
+      q.remove();
+    }
+  }
+}
