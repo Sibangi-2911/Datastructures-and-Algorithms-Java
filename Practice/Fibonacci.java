32cf164cfd58c4b2efb60ff45c4a0ddd52ff24ed
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-03-29 20:02:27 +0530

    Code for fibonacci sequence

---
+++ Practice/Fibonacci.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Fibonacci{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number for fibonacci sequence:");
+    int num = sc.nextInt();
+    int a = 0;
+    int b =1;
+    for(int i=1;i<=num;i++){
+      System.out.print(a+" ");
+      int c = a+b;
+      a = b;
+      b = c;
+    }
+  }
+}
