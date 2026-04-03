30e1b4cea580f45d026e17e2cffa1df5c4ceb72e
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-04-03 11:40:35 +0530

    Updated the code to find second largest element in an array

--- Practice/SecondLargestNumArray.java
+++ Practice/SecondLargestNumArray.java
@@ -1,16 +1,21 @@
 public class SecondLargestNumArray {
   public static void main(String args[]){
-    int arr[] = {10,25,5,40};
+    int arr[] = {25,25,25,25};
     int firstLargest = Integer.MIN_VALUE;
-    int secondLargest = Integer.MAX_VALUE;
+    int secondLargest = Integer.MIN_VALUE;
     for(int num:arr){
       if(num>firstLargest){
         secondLargest = firstLargest;
         firstLargest = num;
       } else if(num>secondLargest && num!=firstLargest){
         secondLargest=num;
-      }
     }
+  }
+  if(secondLargest == Integer.MIN_VALUE){
+        System.out.println("No second largest element");
+  }
+  else{
     System.out.println("Second Largest Element in the array : "+secondLargest);
   }
+}
 }
