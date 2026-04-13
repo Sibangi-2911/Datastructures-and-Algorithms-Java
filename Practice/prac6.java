d6db8d32cab76da7d2dab0c94c49f90c96bd5544
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-04-13 20:56:07 +0530

    Find frequency of characters in string

---
+++ Practice/prac6.java
@@ -0,0 +1,13 @@
+//Find frequency of characters in string
+import java.util.*;
+public class prac6 {
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    Map<Character, Integer> map = new HashMap<>();
+    for(char ch: str.toCharArray()){
+      map.put(ch, map.getOrDefault(ch,0)+1);
+    }
+    System.out.println(map);
+  }
+}
